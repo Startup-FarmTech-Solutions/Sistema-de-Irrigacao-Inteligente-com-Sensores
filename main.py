50b6ca45d7ec9d690280f89dc74c5551b537e440
yggdrasilGit <168605322+yggdrasilGit@users.noreply.github.com>
2025-05-20 20:24:31 -0300

    Incluiu docstring formam retirados pacotes desnecessarios
    A estrutura da arquitetura esta diferente estamos utilizando a insersao automatica no banco de dados, mas separando as aplicacoes para cada aplicacao

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
-import os
-from dotenv import load_dotenv
-from fastapi import FastAPI
-from pydantic import BaseModel
-from datetime import datetime
+# -*- coding: utf-8 -*-
+
+# importando as bibliotecas necessárias
 from connection.connection_db import ConnectionDB
 from controller.area_plantio_controller import AreaPlantioController
 from controller.cultura_controller import CulturaController
 from controller.leitura_sensor_controller import LeituraSensorController
 from controller.sensor_controller import SensorController
-from sensor_solo.main import main
-from dashboard.app import main
+
+
+
+"""
+    Este arquivo é o ponto de entrada principal do sistema de irrigação inteligente.
+    Ele realiza o carregamento das variáveis de ambiente e inicializa a conexão com o banco de dados Oracle.
+
+    Arquitetura do sistema: Microserviços
+    - Cada microserviço é responsável por uma funcionalidade específica do sistema.
+
+    Principais componentes:
+    1. Sensor Solo: Coleta dados dos sensores de umidade do solo e armazena no banco de dados.
+    2. Dashboard: Exibe, em tempo real, os dados coletados pelos sensores em um painel web.
+    3. API: Fornece endpoints para interação externa com o sistema (ex: cadastro de áreas, sensores, culturas).
+
+    Observação: 
+    - É necessário criar um ambiente virtual (.venv) em cada pasta de microserviço para instalar as dependências específicas de cada um.
+
+    Para iniciar o sistema, execute este arquivo.
+"""
 
 if __name__ == "__main__":
     connection = ConnectionDB()
     connection.connect_to_oracle() # Conecte ao banco de dados
-    main()
+    
     
